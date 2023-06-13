5a383857a88cbefa51ebf7ba9b0b3a29094b3630
Pavlov Kostyantyn <realanka77@gmail.com>
2023-06-13 11:52:15 +0300

    feat: resolve cli: env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+    const env = process.env;
+    const res = [];
+    for (const key in env) {
+        if (Object.hasOwnProperty.call(env, key)) {
+            const element = env[key];
+            if (key.startsWith('RSS')) {
+                res.push(key + '=' + element);
+            }
+        }
+    }
+    console.log(res.join('; '));
 };
 
 parseEnv();

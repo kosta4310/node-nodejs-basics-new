c2e668f54830982515f457b1a8cd2a09be7d4c02
Pavlov Kostyantyn <realanka77@gmail.com>
2023-06-13 11:44:57 +0300

    feat: resolve cli: args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    const arrArgs = process.argv;
+    const res = [];
+    for (let i = 2; i < arrArgs.length; i++) {
+        const element = arrArgs[i];
+        if (element.startsWith('--')) {
+            res.push(`${element.slice(2)} is ${arrArgs[i + 1]}`);
+        }
+    } 
+    console.log(res.join(', '));
 };
 
 parseArgs();

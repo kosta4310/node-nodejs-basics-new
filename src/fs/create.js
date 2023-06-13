1dafd4b01af2b5618a72367dc51474312eaba158
Pavlov Kostyantyn <realanka77@gmail.com>
2023-06-13 12:07:08 +0300

    feat: resolve fs create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import {writeFile} from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here 
+    const data = 'I am fresh and young';
+    try {
+        await writeFile('./src/fs/files/fresh.txt', data, {flag: 'wx'});
+    } catch (error) {
+        if (error.code === 'EEXIST') {
+            throw new Error('FS operation failed');
+        }
+    } 
 };
 
 await create();

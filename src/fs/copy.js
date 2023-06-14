def80c22f5d4707878ecc4db4eaff540529c14c5
Pavlov Kostyantyn <realanka77@gmail.com>
2023-06-14 22:02:31 +0300

    feat: resolve fs copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import {mkdir, readdir, copyFile, stat} from 'node:fs/promises';
+
+import url from 'node:url';
+import path from 'node:path';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    const src = path.resolve(__dirname, './files');
+    const dest = path.resolve(__dirname, './files_copy');
+    try {
+        const files = await readdir(src);
+        await mkdir(dest);
+        for (const file of files) {
+            
+            copyFile(path.resolve(src, file), path.resolve(dest, file));
+        }
+    } catch (error) {
+        if (error.code === 'EEXIST' || error.code === 'ENOENT') {
+            
+            throw new Error('FS operation failed');
+        }
+    }
 };
 
 await copy();

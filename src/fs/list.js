1876ab77ee2ac1208b621f22ec23f14861fbb446
Pavlov Kostyantyn <realanka77@gmail.com>
2023-06-14 22:33:14 +0300

    feat: resolve fs list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import {readdir} from 'node:fs/promises';
+
+import url from 'node:url';
+import path from 'node:path';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const folder = path.resolve(__dirname, './files');
+    try {
+        const dirents = await readdir(folder, {withFileTypes: true});
+        for (const dirent of dirents) {
+            if (dirent.isFile) {
+                console.log(dirent.name);
+            }
+        }
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+    }
 };
 
 await list();

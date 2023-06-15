3442455f93db8f54f7f604ae5d5b9755c0b36cf8
Pavlov Kostyantyn <realanka77@gmail.com>
2023-06-15 21:54:53 +0300

    refactor: clean code

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,10 +1,5 @@
 import {} from 'node:fs';
-import url from 'node:url';
-import path from 'node:path';
 import {EOL} from 'node:os';
-
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const transform = async () => {
     process.stdin.on('data', (data) => {

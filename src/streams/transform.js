445e7875f3e9c60d406d57677845ebb745e44a51
Pavlov Kostyantyn <realanka77@gmail.com>
2023-06-15 21:52:09 +0300

    feat: add stream transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import {} from 'node:fs';
+import url from 'node:url';
+import path from 'node:path';
+import {EOL} from 'node:os';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const transform = async () => {
-    // Write your code here 
+    process.stdin.on('data', (data) => {
+        if(data !== null) {
+            const context = data.toString();
+            process.stdout.write(context.split('').reverse().join('') + EOL);
+        }
+    })
 };
 
 await transform();

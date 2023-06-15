a46915bed92c29dcfaf1fcaec18a3c596318dbfa
Pavlov Kostyantyn <realanka77@gmail.com>
2023-06-15 22:20:09 +0300

    feat: add zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import {createReadStream, createWriteStream} from 'fs';
+import {createGzip} from 'node:zlib';
+import url from 'node:url';
+import path from 'node:path';
+import { pipeline } from 'stream';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const pathToSource = path.resolve(__dirname, './files/fileToCompress.txt');
+    const pathToDestination = path.resolve(__dirname, './files/archive.gz');
+    const readStream = createReadStream(pathToSource);
+    const writeStream = createWriteStream(pathToDestination);
+
+    pipeline(
+        readStream, gzip, writeStream, (error) => {
+            if (error) {
+                throw new Error('FS operation failed');
+            }
+        }
+    )
 };
 
 await compress();

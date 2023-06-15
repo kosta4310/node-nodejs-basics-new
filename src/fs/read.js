0063dab3786d3823dffad05ced0604bddd2c745e
Pavlov Kostyantyn <realanka77@gmail.com>
2023-06-15 10:08:48 +0300

    feat: add fs read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import {readFile} from 'node:fs/promises';
+
+import url from 'node:url';
+import path from 'node:path';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const file = path.resolve(__dirname, './files/fileToRead.txt');
+    try {
+        const data = await readFile(file, {encoding: 'utf-8'});
+        console.log(data);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+    }
 };
 
 await read();
